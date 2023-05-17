bc13f5d036d61370b39448bad10f144e229e3221
Mesiano, Cristian <32989204+KBRIEL@users.noreply.github.com>
2023-05-17 20:27:14 -0300

    Update main.py
    frutas

--- main.py
+++ main.py
@@ -9,7 +9,51 @@ class Msg(BaseModel):
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+    return {"message": " Welcome to Del Origen!"}
+
+@app.get("/frutas")
+async def frutas():
+    return [
+    {
+      "precio": 500,
+      "id": 1,
+      "title": "Manzanas",
+      "thumbnailUrl": "./img/Manzanas.jpg"
+    },
+    {
+      "precio ": 300,
+      "id": 2,
+      "title": "Peras",
+      "hola":"hola1",
+      "thumbnailUrl": "./img/pera.jpg"
+    },
+    {
+      "precio": 100,
+      "id": 3,
+      "title": "Espinaca",
+      "hola":"Hola2",
+      "thumbnailUrl": "./img/espinaca.jpg"
+    },
+    {
+      "precio": 50,
+      "id": 4,
+      "title": "Sandía",
+      "thumbnailUrl": "./img/sandia.jpg"
+    },
+    {
+      "precio": 100,
+      "id": 5,
+      "title": "Mango",
+      "thumbnailUrl": "./img/mango.jpg"
+    },
+    {
+      "precio": 150,
+      "id": 6,
+      "title": "Papas",
+      "thumbnailUrl": "./img/papas.jpg"
+    }
+  ]
+
 
 
 @app.get("/path")
